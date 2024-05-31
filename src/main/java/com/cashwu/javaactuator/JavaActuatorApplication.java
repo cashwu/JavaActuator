077862dcfcbc338a4377f977e8955c42e09aad46
Cash <cashwugeek@icloud.com>
2024-05-31 10:16:25 +0800

    init

---
+++ src/main/java/com/cashwu/javaactuator/JavaActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaActuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashwu/javaactuator/JavaActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashwu.javaactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
